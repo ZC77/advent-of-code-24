28a1862e5ba29d15162e5921025c4ea322b6615e
Zachary Cui <zacharycui2009@gmail.com>
2024-12-03 00:47:54 +1300

    day 2 pt.2 WIP

--- day2.js
+++ day2.js
@@ -40,4 +40,33 @@ fs.readFile("./day2_input.txt", "utf8", (err, data) => {
 
   const numOfSafeReports = reports.reduce((acc, curr) => (curr ? (acc += 1) : (acc += 0)), 0);
   console.log(numOfSafeReports);
+
+  /** PART 2 */
+  // Same rules apply as before, but we can remove a single datapoint to make it compliant.
+
+  //
+
+  const part2 = arrayified.map((arr) => {
+    const initial = arr
+      .map((val, idx, thisArray) => {
+        const hasNext = idx < thisArray.length - 1;
+        if (hasNext) {
+          const next = thisArray[idx + 1];
+          const difference = val - next;
+          return difference;
+        }
+        return undefined; // last value has nothing to compare to
+      })
+      .slice(0, -1);
+
+    let hasRemoved = false;
+
+    initial.map((val, idx, thisArray) => {
+      const withinRange = Math.abs(val) >= 1 && Math.abs(val) <= 3;
+      if (!withinRange && !hasRemoved) {
+        hasRemoved = true;
+        return undefined;
+      }
+    });
+  });
 });

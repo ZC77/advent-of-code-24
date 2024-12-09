cc7fba45dde18bea5fd165829e0b2bf15842a365
ZC77 <zacharycui2009@gmail.com>
2024-12-09 21:44:52 +1300

    day2 p2 WIP still

--- day2.js
+++ day2.js
@@ -1,7 +1,8 @@
 const fs = require("fs");
 
-fs.readFile("./day2_mini.txt", "utf8", (err, data) => {
-  const splitData = data.split("\r\n");
+fs.readFile("./day2_edge.txt", "utf8", (err, data) => {
+  const splitData = data.split("\n");
+  // const splitData = data.split("\r\n"); // WINDOWS
 
   // Conditions of a safe report
   // Either all increasing or decreasing.
@@ -91,136 +92,113 @@ fs.readFile("./day2_mini.txt", "utf8", (err, data) => {
   // const part2Reports = part2.reduce((acc, curr) => (curr ? (acc += 1) : (acc += 0)), 0);
   // console.log(`Part2 reports: ${part2Reports}`);
 
-  // const getDifference = (val1, val2) => {
-  //   const difference = Math.abs(val1 - val2);
-  //   return { legal: difference >= 1 && difference <= 3, value: val1 - val2 };
-  // };
+  const getDifference = (val1, val2) => {
+    const difference = Math.abs(val1 - val2);
+    return { legal: difference >= 1 && difference <= 3, value: val1 - val2 };
+  };
 
-  // const hasSameSign = (val1, val2) => {
-  //   return Math.sign(val1) == Math.sign(val2);
-  // };
+  const hasSameSign = (val1, val2) => {
+    return Math.sign(val1) == Math.sign(val2);
+  };
 
-  // const part2algo2 = arrayified.map((arr, idxMain) => {
-  //   // Sliding window with +1 lookahead
-  //   // Same algo as before, compared curr with next to check polarity and difference are within bounds
-  //   // Establish a flag to track that a value has been removed
-  //   // If a value is illegal, extend window to check next value
-  //   // Check difference between those two, if defined and OK
+  const part2algo2 = arrayified.map((arr, idxMain) => {
+    // Sliding window with +1 lookahead
+    // Same algo as before, compared curr with next to check polarity and difference are within bounds
+    // Establish a flag to track that a value has been removed
+    // If a value is illegal, extend window to check next value
+    // Check difference between those two, if defined and OK
 
-  //   let hasRemoved = false;
-  //   let removeNext = false;
-  //   let polarity;
+    let hasRemoved = false;
+    let removeNext = false;
+    let polarity;
 
-  //   const processed = arr.map((val, idx, thisArray) => {
-  //     if (removeNext) {
-  //       removeNext = false;
-  //       return true;
-  //     }
-  //     const hasNext = idx < thisArray.length - 1;
-  //     if (hasNext) {
-  //       const next = thisArray[idx + 1];
-  //       polarity = Math.sign(val - thisArray[idx + 1]);
-  //       const { legal } = getDifference(val, next);
+    const processed = arr.map((val, idx, thisArray) => {
+      if (hasRemoved && removeNext) {
+        removeNext = false;
+        return undefined;
+      }
+      if (idx == 0) {
+        // first comparison - special case
+        // we take 3 polarity checks to determine the overall polarity
+        const polarity1 = Math.sign(val - thisArray[1]);
+        const polarity2 = Math.sign(thisArray[1] - thisArray[2]);
+        const polarity3 = Math.sign(thisArray[2] - thisArray[3]);
 
-  //       if (legal && sameSign) {
-  //         return true;
-  //       } else {
-  //         if (!hasRemoved) {
-  //           // We haven't removed, so lets try if it would make a difference
-  //           const existsOneAhead = idx < thisArray.length - 2;
-  //           if (existsOneAhead) {
-  //             const { legal: legalOneAhead } = getDifference(val, thisArray[idx + 2]);
-  //             console.log(`legalOneAhead: ${legalOneAhead}`);
-  //             const hasSameSignOneAhead = polarity == Math.sign(val - thisArray[idx + 2]);
-  //             console.log(`hasSameSignOneAhead: ${legalOneAhead}`);
-  //             if (legalOneAhead && hasSameSignOneAhead) {
-  //               console.log(`at ${idx} removing next... `);
-  //               hasRemoved = true;
-  //               removeNext = true;
-  //               return true;
-  //             }
-  //           }
-  //         }
-  //         // not going to be safe.
-  //         return false;
-  //       }
-  //     }
-  //     return undefined;
-  //   });
-
-  //   const removeUndefined = processed.filter((val) => val != undefined);
-  //   console.log(removeUndefined);
-
-  //   return removeUndefined.reduce((acc, curr) => acc && curr);
-  // });
-
-  // const final = part2algo2.reduce((acc, curr) => (curr ? acc + 1 : acc), 0);
-
-  // console.log(final);
-
-  const isNextLegal = (curr, next) => (next > curr && curr + 4 < next) || (next <= curr - 1 && next > curr - 4);
-  const attempt3 = arrayified.map((arr, idxMain) => {
-    let acc = JSON.parse(JSON.stringify([arr[0]]));
-    let input = JSON.parse(JSON.stringify(arr.slice(1)));
-    console.log(`acc: ${acc} input: ${input}`);
-    let polarity = undefined;
-    let hasSkipped = false;
-    let secondProblem = false;
-
-    while (!secondProblem && input.length > 0) {
-      // While we have input
-      const curr = acc[acc.length - 1];
-      const next = JSON.parse(JSON.stringify(input[0]));
-      console.log(acc);
-
-      if (acc.length == 2) {
-        polarity = Math.sign(acc[0] - acc[1]);
+        polarity = [polarity1, polarity2, polarity3].reduce((acc, curr) => acc + curr, 0);
       }
 
-      const legal = isNextLegal(curr, next);
-      console.log(`legal: ${legal}, curr: ${curr}, next: ${next}`);
+      // Begin regular checks
+      const hasNext = idx < thisArray.length - 1;
 
-      if (legal) {
-        if (polarity == undefined) {
-          acc = JSON.parse(JSON.stringify([...acc, next]));
-        }
-        if (polarity != undefined) {
-          // We've established polarity from having at least two numbers
-          const incomingPolarityCheck = polarity == Math.sign(acc[acc.length - 1] - next);
+      if (hasNext) {
+        const next = thisArray[idx + 1];
+        const { legal: isLegalDifference } = getDifference(val, next);
+        const currentPolarity = Math.sign(val - next);
+        const isLegalPolarity = currentPolarity == Math.sign(polarity);
+        const isLegal = isLegalDifference && isLegalPolarity;
 
-          if (incomingPolarityCheck) {
-            // Passing number, so allow it through
-            acc = JSON.parse(JSON.stringify([...acc, next]));
-            input.shift(1);
-          } else if (!incomingPolarityCheck && !hasSkipped) {
-            // First time removing, so remove it
-            input.shift(1);
-            hasSkipped = true;
-          } else {
-            // Ok there's a second problem now, so lets stop
-            secondProblem = true;
+        if (isLegal) {
+          return val;
+        } else if (!isLegal) {
+          if (!hasRemoved) {
+            console.log(`issues with ${val} and ${next}`);
+            // Attempt to remove this number
+            // We can remove directly if it's the first and last number
+            const isFirstOrLast = idx == 0 || idx == thisArray.length - 1;
+
+            if (!isFirstOrLast) {
+              console.log("its not first or last");
+              // Check we can piece it back together without issues
+              const before = thisArray[idx - 1];
+              const newSeqPolarityLHS = Math.sign(before - next);
+              const { legal: isNewSeqLegalLHS } = getDifference(before, next);
+              console.log(`legal: ${isNewSeqLegalLHS}`);
+
+              const newSeqPolarityRHS = Math.sign(val - next);
+              const { legal: isNewSeqLegalRHS } = getDifference(val, next);
+              const isNewSeqLegalPolarityLHS = newSeqPolarityLHS == Math.sign(polarity);
+              const isNewSeqLegalPolarityRHS = newSeqPolarityRHS == Math.sign(polarity);
+              if (!isNewSeqLegalLHS) {
+                console.log(`${isNewSeqLegalRHS} ${isNewSeqLegalPolarityRHS}`);
+                // Return false as it won't work anyway.
+                return false;
+              }
+              if (!isNewSeqLegalPolarityLHS) {
+                if (newSeqPolarityLHS >= 1 && polarity < 1) {
+                  removeNext = true;
+                  hasRemoved = true;
+                  return val;
+                } else if (newSeqPolarityLHS < 1 && polarity >= 1) {
+                  return undefined;
+                }
+              }
+            }
+
+            hasRemoved = true;
+            if (!isLegalPolarity && currentPolarity != 0) {
+              // +ve polarity = numbers descending
+              // -ve polarity = numbers ascending
+              if (currentPolarity >= 1 && polarity < 1) {
+                removeNext = true;
+                return val;
+              } else if (currentPolarity < 1 && polarity >= 1) {
+                return undefined;
+              }
+            } else {
+              return undefined;
+            }
           }
+          return false;
         }
-      } else if (!legal && !hasSkipped) {
-        // First time removing, so remove it
-        input.shift(1);
       } else {
-        secondProblem = true;
+        return val;
       }
-    }
+    });
 
-    return acc;
+    return processed;
   });
 
-  console.log(attempt3);
+  const final = part2algo2.map((val) => !val.includes(false)).reduce((acc, curr) => (acc += curr ? 1 : 0), 0);
+  console.log(part2algo2);
+  console.log(final);
 });
-
-// x
-// // (x + 1, x + 2, x + 3 x - 1, x - 2, x - 3
-
-// [1] [1, 3, 1]
-// [1, 3] [1]
-
-// [1] [3, 2, 4, 5]
-// [1, 3] [2, 4 ,5]
-// [1, 3, 4, 5]

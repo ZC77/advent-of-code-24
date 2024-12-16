1ed2adf5422215021534ade8eba28e527840121c
Zachary Cui <zacharycui2009@gmail.com>
2024-12-16 20:48:39 +1300

    day 2 and 4 updates

--- day2.js
+++ day2.js
@@ -1,6 +1,8 @@
 const fs = require("fs");
 
-fs.readFile("./day2_edge.txt", "utf8", (err, data) => {
+// fs.readFile("./day2_edge.txt", "utf8", (err, data) => {
+// fs.readFile("./day2_input.txt", "utf8", (err, data) => {
+fs.readFile("./message.txt", "utf8", (err, data) => {
   const splitData = data.split("\n");
   // const splitData = data.split("\r\n"); // WINDOWS
 
@@ -44,53 +46,6 @@ fs.readFile("./day2_edge.txt", "utf8", (err, data) => {
 
   /** PART 2 */
   // Same rules apply as before, but we can remove a single datapoint to make it compliant.
-
-  //
-
-  // const part2 = arrayified.map((arr, idxMain) => {
-  //   const initial = arr
-  //     .map((val, idx, thisArray) => {
-  //       const hasNext = idx < thisArray.length - 1;
-  //       if (hasNext) {
-  //         const next = thisArray[idx + 1];
-  //         const difference = val - next;
-  //         return difference;
-  //       }
-  //       return undefined; // last value has nothing to compare to
-  //     })
-  //     .slice(0, -1);
-
-  //   let hasRemoved = false;
-
-  //   // Diabolical algorithm
-  //   // Map through the difference array and detect if it's out of range
-  //   // Also detect if the sign is an issue.
-  //   // If out of range and haven't REMOVED BEFORE, remove it from the array and set hasRemoved to true
-  //   // Otherwise just return the value. We don't care if it can't be saved.
-  //   const processed = initial.map((val, idx, thisArray) => {
-  //     const withinRange = Math.abs(val) >= 1 && Math.abs(val) <= 3;
-  //     const sameSign = Math.sign(val) == Math.sign(thisArray[0]);
-  //     if ((!sameSign || !withinRange) && !hasRemoved) {
-  //       console.log(`removing unsafe difference ${val} from line ${idxMain + 1}`);
-  //       hasRemoved = true;
-  //       return undefined;
-  //     }
-  //     return val;
-  //   });
-
-  //   console.log(processed);
-
-  //   return processed
-  //     .filter((val) => val != undefined)
-  //     .reduce(
-  //       (acc, curr, idx, thisArray) =>
-  //         acc && Math.abs(curr) >= 1 && Math.abs(curr) <= 3 && Math.sign(curr) == Math.sign(thisArray[0]),
-  //       true
-  //     );
-  // });
-
-  // const part2Reports = part2.reduce((acc, curr) => (curr ? (acc += 1) : (acc += 0)), 0);
-  // console.log(`Part2 reports: ${part2Reports}`);
 
   const getDifference = (val1, val2) => {
     const difference = Math.abs(val1 - val2);
--- day4.js
+++ day4.js
@@ -1,3 +1,30 @@
 const fs = require("fs");
 
-fs.readFile("./day4_input.txt", "utf8", (err, data) => {});
+fs.readFile("./day4_input.txt", "utf8", (err, data) => {
+  const WIDTH = 139; // Max array index - Width AND Height of array.
+  const horizontalRegex = /(XMAS)|(SAMX)/g;
+
+  const cells = data
+    .split("\r\n")
+    .map((val) => val.split(""))
+    .flat();
+
+  const horizontalCount = data
+    .split("\r\n")
+    .map((val) => val.match(horizontalRegex))
+    .flat()
+    .filter((val) => val !== null && val !== undefined);
+
+  let verticalCount;
+
+  for (x = 0; x < WIDTH; x++) {
+    let verticalString = "";
+    for (y = 0; y < WIDTH; y++) {
+      verticalString += cells[y * 140];
+    }
+    const count = verticalString.match(horizontalRegex).flat();
+    console.log(verticalString);
+  }
+
+  console.log(horizontalCount.length);
+});

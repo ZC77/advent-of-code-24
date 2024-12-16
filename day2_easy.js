ecca80c5ea0d48eb46e077467be4f12be3264956
Zachary Cui <zacharycui2009@gmail.com>
2024-12-16 21:52:29 +1300

    day 2 easy algo

--- day2_easy.js
+++ day2_easy.js
@@ -1,10 +1,10 @@
 const fs = require("fs");
 
 // fs.readFile("./day2_edge.txt", "utf8", (err, data) => {
-// fs.readFile("./day2_input.txt", "utf8", (err, data) => {
-fs.readFile("./message.txt", "utf8", (err, data) => {
-  const splitData = data.split("\n");
-  // const splitData = data.split("\r\n"); // WINDOWS
+fs.readFile("./day2_input.txt", "utf8", (err, data) => {
+  // fs.readFile("./message.txt", "utf8", (err, data) => {
+  // const splitData = data.split("\n");
+  const splitData = data.split("\r\n"); // WINDOWS
 
   // Conditions of a safe report
   // Either all increasing or decreasing.
@@ -45,12 +45,12 @@ fs.readFile("./message.txt", "utf8", (err, data) => {
   // console.log(numOfSafeReports);
 
   /** PART 2 */
-  /** NOT WORKING */
   // Same rules apply as before, but we can remove a single datapoint to make it compliant.
 
   const getDifference = (val1, val2) => {
     const difference = Math.abs(val1 - val2);
-    return { legal: difference >= 1 && difference <= 3, value: val1 - val2 };
+    const polarity = Math.sign(val1 - val2);
+    return { legal: difference >= 1 && difference <= 3, polarity };
   };
 
   const hasSameSign = (val1, val2) => {
@@ -64,15 +64,9 @@ fs.readFile("./message.txt", "utf8", (err, data) => {
     // If a value is illegal, extend window to check next value
     // Check difference between those two, if defined and OK
 
-    let hasRemoved = false;
-    let removeNext = false;
     let polarity;
 
     const processed = arr.map((val, idx, thisArray) => {
-      if (hasRemoved && removeNext) {
-        removeNext = false;
-        return undefined;
-      }
       if (idx == 0) {
         // first comparison - special case
         // we take 3 polarity checks to determine the overall polarity
@@ -80,48 +74,25 @@ fs.readFile("./message.txt", "utf8", (err, data) => {
         const polarity2 = Math.sign(thisArray[1] - thisArray[2]);
         const polarity3 = Math.sign(thisArray[2] - thisArray[3]);
 
-        polarity = [polarity1, polarity2, polarity3].reduce((acc, curr) => acc + curr, 0);
+        polarity = Math.sign(polarity1 + polarity2 + polarity3);
       }
 
-      // Begin regular checks
-      const hasNext = idx < thisArray.length - 1;
+      const removeSelf = [...thisArray].filter((_, filterFnIdx) => filterFnIdx != idx);
 
-      if (hasNext) {
-        const next = thisArray[idx + 1];
-        const { legal: isLegalDifference } = getDifference(val, next);
-        const currentPolarity = Math.sign(val - next);
-        const isLegalPolarity = currentPolarity == Math.sign(polarity);
-        const isLegal = isLegalDifference && isLegalPolarity;
-
-        if (isLegal) {
-          return val;
-        } else if (!isLegal) {
-          if (!hasRemoved) {
-            console.log(`issues with ${val} and ${next}`);
-            // Attempt to remove this number
-            // First - can we fix by removing adjacent values?
-            if (!isLegalDifference) {
-              // A non-legal adjacent difference - try skip values
-              const before = thisArray[idx - 1];
-              const skipPolarity = Math.sign(before - next);
-              const { legal: isSkipDifferenceLegal } = getDifference(before, next);
-              const isSkipPolarityLegal = skipPolarity == Math.sign(polarity);
-
-              if (isSkipDifferenceLegal) {
-              }
-            } else {
-              // Legal adjacent difference - try removing the one with bad polarity
-            }
-          }
-          return false;
+      const result = removeSelf.map((val, idx, self) => {
+        if (idx == self.length - 1) {
+          // last value, it doesn't compare with anything
+          return true;
         }
-      }
+        const { legal, polarity: localPolarity } = getDifference(val, self[idx + 1]);
+        return legal && polarity == localPolarity;
+      });
+
+      return result.reduce((acc, curr) => acc && curr, true);
     });
 
-    return processed;
+    return processed.some((val) => val);
   });
 
-  const final = part2algo2.map((val) => !val.includes(false)).reduce((acc, curr) => (acc += curr ? 1 : 0), 0);
-  console.log(part2algo2);
-  console.log(final);
+  console.log(part2algo2.reduce((acc, curr) => (curr ? (acc += 1) : (acc += 0)), 0));
 });
